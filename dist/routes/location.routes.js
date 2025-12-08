96dea2d943e91ca0072d9c532619512ad11d99ae
jitendra2007354 <jitendrasinghchauhan3008@gmail.com>
2025-12-08 22:12:57 +0530

    Delete dist directory

--- dist/routes/location.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const location_controller_1 = require("../controllers/location.controller");
-const router = (0, express_1.Router)();
-// Route to get all current driver locations
-// GET /api/location/drivers
-router.get('/drivers', location_controller_1.getDriversLocationController);
-exports.default = router;
